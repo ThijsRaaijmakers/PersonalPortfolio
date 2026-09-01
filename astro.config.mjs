8dc62a089d9f4d0dd49be2fb200680944a54d64d
Thijs <traaijmakers32@gmail.com>
2026-09-01 15:48:02 +0200

    Added XML sitemap for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thijsraaijmakers.me',
   output: 'static',
   adapter: vercel(),
+  integrations: [sitemap()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "nl"],
